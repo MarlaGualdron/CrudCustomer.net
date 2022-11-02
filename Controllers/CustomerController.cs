79fea2f5254820f19e1941f7806c27ea66e29b04
Marla Gualdron <71885886+MarlaGualdron@users.noreply.github.com>
2022-11-02 13:36:31 -0300

    controller added and compose fixed

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,35 @@
+using CrudCustomers.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrudCustomers.Api.Controllers;
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class CustomerController : ControllerBase
+    {
+        private readonly ApiDbContext _context;
+
+        public CustomerController(ApiDbContext context)
+        {
+            _context = context;
+        }
+
+    [HttpGet]
+
+    public object Get()
+    {
+        return _context.Customers.Where(b => b.Full_name.Contains("FullName")).Select((c) => new {
+            Id = c.Id,
+            Full_name = c.Full_name,
+            Date_of_birth = c.Date_of_birth,
+            Gender = c.Gender,
+            Cuil = c.Cuil,
+            Phone = c.Phone,
+            Deleted = c.Deleted
+
+        }).ToList();
+    }
+}
+
